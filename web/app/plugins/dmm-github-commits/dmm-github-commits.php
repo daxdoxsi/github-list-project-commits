40e1e28d6e9ff61d2037d385bac5a3341eb890ba
David M. Miller <daxdoxsi@gmail.coom>
2024-04-26 18:58:46 -0600

    Set up the Plugin header configuration

---
+++ web/app/plugins/dmm-github-commits/dmm-github-commits.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin name: GitHub Repository Commits
+ * Description: List the commits to the repository of the current project
+ * Author: David M. Miller
+ * Author URI: https://www.davidm.website/
+ * Version: 0.0.1
+ */
+
